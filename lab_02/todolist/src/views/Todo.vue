6ca0fcb26d59c5fc5702e23492acdf624485cbce
Sonechek <flex.tlex@gmail.com>
2023-12-02 14:04:56 +0500

    main

--- lab_02/todolist/src/views/Todo.vue
+++ lab_02/todolist/src/views/Todo.vue
@@ -3,7 +3,16 @@
 </script>
 <template>
 
-Это страничка: Todo
+<div>
+    <h2>Add Note</h2>
+    <form @submit.prevent="createNote">
+      <input type="text" v-model="title" placeholder="Note Title" />
+      <br />
+      <textarea v-model="content" placeholder="Note Content"></textarea>
+      <br />
+      <button type="submit">Create Note</button>
+    </form>
+  </div>
 </template>
 <script>
 // Здесь исходный код компонента
